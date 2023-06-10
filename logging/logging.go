72af346faf404e9e9d9adbcd73305428c155c563
abrahamhwj <abrahamhwj@qq.com>
2023-06-10 22:44:12 +0800

    config & logging module init

--- logging/logging.go
+++ logging/logging.go
@@ -40,7 +40,7 @@ func init() {
 		level = loggingLevel[LevelDebug]
 	}
 	// Set klog log level
-	flag.Set("v", strconv.Itoa(int(level)))
+	_ = flag.CommandLine.Set("v", strconv.Itoa(int(level)))
 }
 
 // Logging struct
